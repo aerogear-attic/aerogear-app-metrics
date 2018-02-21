ac9ab9c514e6cb4e56529b66b113b6bd599806a9
Ali Ok <aliok@apache.org>
2018-02-21 10:56:02 +0300

    AEROGEAR-2044 - use Testify's assertions

--- pkg/web/metricsHandler_test.go
+++ pkg/web/metricsHandler_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aerogear/aerogear-metrics-api/pkg/mobile"
 	"bytes"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/assert"
 	"github.com/pkg/errors"
 )
 
@@ -46,10 +47,7 @@ func TestMetricsEndpointShouldPassReceivedDataToMetricsService(t *testing.T) {
 
 	byteBuffer := new(bytes.Buffer)
 	err := json.NewEncoder(byteBuffer).Encode(metric)
-
-	if err != nil {
-		t.Fatal("unable to marshal metric", err)
-	}
+	assert.Nil(t, err, "did not expect an error marshaling metric")
 
 	mockMetricsService := new(MockMetricsService)
 	mockMetricsService.On("Create", metric).Return(metric, nil)
@@ -58,16 +56,12 @@ func TestMetricsEndpointShouldPassReceivedDataToMetricsService(t *testing.T) {
 	defer s.Close()
 
 	res, err := http.Post(s.URL+"/metrics", "application/json", byteBuffer)
-	if err != nil {
-		t.Fatal("did not expect an error posting metrics", err)
-	}
+	assert.Nil(t, err, "did not expect an error posting metrics")
 
 	defer res.Body.Close()
 	_, err = ioutil.ReadAll(res.Body)
 
-	if res.StatusCode != 200 {
-		t.Fatal("expected a 200 status")
-	}
+	assert.Equal(t, 200, res.StatusCode)
 
 	mockMetricsService.AssertExpectations(t)
 }
@@ -89,28 +83,21 @@ func TestMetricsEndpointShouldReturn500WhenThereIsAnErrorInMetricsService(t *tes
 
 	byteBuffer := new(bytes.Buffer)
 	err := json.NewEncoder(byteBuffer).Encode(metric)
-
-	if err != nil {
-		t.Fatal("unable to marshal metric", err)
-	}
+	assert.Nil(t, err, "did not expect an error marshaling metric")
 
 	mockMetricsService := new(MockMetricsService)
-	mockMetricsService.On("Create", metric).Return(mobile.Metric{}, errors.New("Metrics service error!"))		// mock the service so that it returns an error
+	mockMetricsService.On("Create", metric).Return(mobile.Metric{}, errors.New("Metrics service error!")) // mock the service so that it returns an error
 
 	s := setupMetricsHandler(mockMetricsService)
 	defer s.Close()
 
 	res, err := http.Post(s.URL+"/metrics", "application/json", byteBuffer)
-	if err != nil {
-		t.Fatal("did not expect an error posting metrics", err)
-	}
+	assert.Nil(t, err, "did not expect an error posting metrics")
 
 	defer res.Body.Close()
 	_, err = ioutil.ReadAll(res.Body)
 
-	if res.StatusCode != 500 {
-		t.Fatal("expected a 500 status")
-	}
+	assert.Equal(t, 500, res.StatusCode)
 
 	mockMetricsService.AssertExpectations(t)
 }
@@ -122,16 +109,12 @@ func TestMetricsEndpointShouldNotInteractWithMetricsServiceWhenRequestBodyIsEmpt
 	defer s.Close()
 
 	res, err := http.Post(s.URL+"/metrics", "application/json", nil) // empty request body
-	if err != nil {
-		t.Fatal("did not expect an error posting metrics", err)
-	}
+	assert.Nil(t, err, "did not expect an error posting metrics")
 
 	defer res.Body.Close()
 	_, err = ioutil.ReadAll(res.Body)
 
-	if res.StatusCode != 400 {
-		t.Fatal("expected a 400 status")
-	}
+	assert.Equal(t, 400, res.StatusCode)
 
 	mockMetricsService.AssertNotCalled(t, "Create")
 }
@@ -146,16 +129,12 @@ func TestMetricsEndpointShouldNotInteractWithMetricsServiceWhenRequestBodyIsInva
 	byteBuffer.WriteString("nonsense") // invalid JSON
 
 	res, err := http.Post(s.URL+"/metrics", "application/json", byteBuffer)
-	if err != nil {
-		t.Fatal("did not expect an error posting metrics", err)
-	}
+	assert.Nil(t, err, "did not expect an error posting metrics")
 
 	defer res.Body.Close()
 	_, err = ioutil.ReadAll(res.Body)
 
-	if res.StatusCode != 400 {
-		t.Fatal("expected a 400 status")
-	}
+	assert.Equal(t, 400, res.StatusCode)
 
 	mockMetricsService.AssertNotCalled(t, "Create")
 }
