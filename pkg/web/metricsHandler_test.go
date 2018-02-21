5d3b73cdd8a7e3023a9dba29936a93034d00e071
Ali Ok <aliok@apache.org>
2018-02-21 10:47:47 +0300

    AEROGEAR-2044 - add test case when there is an impl error in MetricsService (http 500)

--- pkg/web/metricsHandler_test.go
+++ pkg/web/metricsHandler_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aerogear/aerogear-metrics-api/pkg/mobile"
 	"bytes"
 	"github.com/stretchr/testify/mock"
+	"github.com/pkg/errors"
 )
 
 type MockMetricsService struct {
@@ -65,7 +66,50 @@ func TestMetricsEndpointShouldPassReceivedDataToMetricsService(t *testing.T) {
 	_, err = ioutil.ReadAll(res.Body)
 
 	if res.StatusCode != 200 {
-		t.Fatal("expected an 200 status")
+		t.Fatal("expected a 200 status")
+	}
+
+	mockMetricsService.AssertExpectations(t)
+}
+
+func TestMetricsEndpointShouldReturn500WhenThereIsAnErrorInMetricsService(t *testing.T) {
+	metric := mobile.Metric{
+		ClientTimestamp: 1234,
+		ClientId:        "client123",
+		App: mobile.AppMetric{
+			ID:         "deadbeef",
+			SDKVersion: "1.2.3",
+			AppVersion: "27",
+		},
+		Device: mobile.DeviceMetric{
+			Platform:        "android",
+			PlatformVersion: "19",
+		},
+	}
+
+	byteBuffer := new(bytes.Buffer)
+	err := json.NewEncoder(byteBuffer).Encode(metric)
+
+	if err != nil {
+		t.Fatal("unable to marshal metric", err)
+	}
+
+	mockMetricsService := new(MockMetricsService)
+	mockMetricsService.On("Create", metric).Return(mobile.Metric{}, errors.New("Metrics service error!"))		// mock the service so that it returns an error
+
+	s := setupMetricsHandler(mockMetricsService)
+	defer s.Close()
+
+	res, err := http.Post(s.URL+"/metrics", "application/json", byteBuffer)
+	if err != nil {
+		t.Fatal("did not expect an error posting metrics", err)
+	}
+
+	defer res.Body.Close()
+	_, err = ioutil.ReadAll(res.Body)
+
+	if res.StatusCode != 500 {
+		t.Fatal("expected a 500 status")
 	}
 
 	mockMetricsService.AssertExpectations(t)
@@ -86,7 +130,7 @@ func TestMetricsEndpointShouldNotInteractWithMetricsServiceWhenRequestBodyIsEmpt
 	_, err = ioutil.ReadAll(res.Body)
 
 	if res.StatusCode != 400 {
-		t.Fatal("expected an 400 status")
+		t.Fatal("expected a 400 status")
 	}
 
 	mockMetricsService.AssertNotCalled(t, "Create")
@@ -110,7 +154,7 @@ func TestMetricsEndpointShouldNotInteractWithMetricsServiceWhenRequestBodyIsInva
 	_, err = ioutil.ReadAll(res.Body)
 
 	if res.StatusCode != 400 {
-		t.Fatal("expected an 400 status")
+		t.Fatal("expected a 400 status")
 	}
 
 	mockMetricsService.AssertNotCalled(t, "Create")
