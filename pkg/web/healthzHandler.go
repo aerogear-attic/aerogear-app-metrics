8d2b9ccf6a5c9cd7d087080a4d53da137b75a02f
Paolo Bueno <paolohaji@gmail.com>
2018-02-21 11:23:58 -0300

    Use `boom` instead of `http.Error`

--- pkg/web/healthzHandler.go
+++ pkg/web/healthzHandler.go
@@ -3,6 +3,8 @@ package web
 import (
 	"net/http"
 	"time"
+
+	"github.com/darahayes/go-boom"
 )
 
 // not reusable type so make it package local
@@ -12,17 +14,20 @@ type healthResponse struct {
 	Status    string    `json:"status"`
 }
 
-type healthHandler struct {
+type HealthHandler struct {
 	pingTarget Pingable
 }
 
-func NewHealthHandler(pingTarget Pingable) *healthHandler {
-	return &healthHandler{
+// NewHealthHandler creates a handler for the Health endpoints
+// pingTarget must contain an implementation that will be utilized to signal that
+// the service is ready to accept requests.
+func NewHealthHandler(pingTarget Pingable) *HealthHandler {
+	return &HealthHandler{
 		pingTarget: pingTarget,
 	}
 }
 
-func (hh *healthHandler) Healthz(w http.ResponseWriter, r *http.Request) {
+func (hh *HealthHandler) Healthz(w http.ResponseWriter, r *http.Request) {
 	status := healthResponse{
 		Timestamp: time.Now().UTC(),
 		Status:    "ok",
@@ -34,10 +39,10 @@ func (hh *healthHandler) Healthz(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (hh *healthHandler) Ping(w http.ResponseWriter, r *http.Request) {
+func (hh *HealthHandler) Ping(w http.ResponseWriter, r *http.Request) {
 	err := hh.pingTarget.Ping()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		boom.ServerUnavailable(w)
 		return
 	}
 
@@ -46,7 +51,7 @@ func (hh *healthHandler) Ping(w http.ResponseWriter, r *http.Request) {
 		Status:    "ok",
 	}
 	if err := withJSON(w, 200, status); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		boom.Internal(w)
 		return
 	}
 }
