f31296466676cc70e6b77a4ecbe39b7647ea7564
Dara Hayes <dara.hayes@redhat.com>
2018-03-01 11:09:02 +0000

    fix: update tests

--- pkg/dao/dao_test.go
+++ pkg/dao/dao_test.go
@@ -3,6 +3,7 @@ package dao
 
 import (
 	"testing"
+	"time"
 
 	"github.com/aerogear/aerogear-app-metrics/pkg/config"
 	"github.com/stretchr/testify/mock"
@@ -81,7 +82,7 @@ func TestCreate(t *testing.T) {
 	clientId := "org.aerogear.metrics.testing"
 	metricsData := []byte("{\"app\":{\"id\":\"com.example.someApp\",\"sdkVersion\":\"2.4.6\",\"appVersion\":\"256\"},\"device\":{\"platform\":\"android\",\"platformVersion\":\"27\"}}")
 
-	err = dao.Create(clientId, metricsData)
+	err = dao.Create(clientId, metricsData, nil)
 
 	if err != nil {
 		t.Errorf("Create() returned an error %s", err.Error())
@@ -103,7 +104,7 @@ func TestCreateBadJSON(t *testing.T) {
 	clientId := "org.aerogear.metrics.testing"
 	metricsData := []byte("InvalidJSON")
 
-	err = dao.Create(clientId, metricsData)
+	err = dao.Create(clientId, metricsData, nil)
 
 	if err == nil {
 		t.Errorf("Create() with invalid JSON did not return an error")
@@ -131,9 +132,38 @@ func TestCreateEmptyClientID(t *testing.T) {
 	clientId := ""
 	metricsData := []byte("{\"app\":{\"id\":\"com.example.someApp\",\"sdkVersion\":\"2.4.6\",\"appVersion\":\"256\"},\"device\":{\"platform\":\"android\",\"platformVersion\":\"27\"}}")
 
-	err = dao.Create(clientId, metricsData)
+	err = dao.Create(clientId, metricsData, nil)
 
 	if err == nil {
 		t.Errorf("Create() with empty clientId did not return an error")
+	}
+}
+
+func TestCreateClientTimestamp(t *testing.T) {
+	config := config.GetConfig()
+	dbHandler := DatabaseHandler{}
+
+	err := dbHandler.Connect(config.DBConnectionString, config.DBMaxConnections)
+
+	if err != nil {
+		t.Errorf("Connect() returned an error: %s", err.Error())
+	}
+
+	dbHandler.DoInitialSetup()
+
+	if err != nil {
+		t.Errorf("Connect() returned an error: %s", err.Error())
+	}
+
+	dao := NewMetricsDAO(dbHandler.DB)
+
+	clientId := "org.aerogear.metrics.testing"
+	metricsData := []byte("{\"app\":{\"id\":\"com.example.someApp\",\"sdkVersion\":\"2.4.6\",\"appVersion\":\"256\"},\"device\":{\"platform\":\"android\",\"platformVersion\":\"27\"}}")
+	time := time.Now()
+
+	err = dao.Create(clientId, metricsData, &time)
+
+	if err != nil {
+		t.Errorf("Create() returned an error %s", err.Error())
 	}
 }
