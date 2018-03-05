69253c6fde956b2a8a78e02874930fe79d9e1768
Dara Hayes <dara.hayes@redhat.com>
2018-03-05 13:17:42 +0000

    feat: initial parsing of security metrics (#33)
    * feat: initial parsing of security metrics
    
    * fix: parse array/slice of security metrics
    
    * fix: perform basic validation on SecurityMetrics
    
    * fix: error messages
    
    * fix: add test cases for security metrics validation
    
    * fix: added Name field to SecurityMetric type
    
    * fix: typo in securityMetricMissingNameError

--- pkg/mobile/types.go
+++ pkg/mobile/types.go
@@ -18,8 +18,9 @@ type Metric struct {
 }
 
 type MetricData struct {
-	App    *AppMetric    `json:"app,omitempty"`
-	Device *DeviceMetric `json:"device,omitempty"`
+	App      *AppMetric       `json:"app,omitempty"`
+	Device   *DeviceMetric    `json:"device,omitempty"`
+	Security *SecurityMetrics `json:"security,omitempty"`
 }
 
 type AppMetric struct {
@@ -33,13 +34,31 @@ type DeviceMetric struct {
 	PlatformVersion string `json:"platformVersion"`
 }
 
+type SecurityMetrics []SecurityMetric
+
+type SecurityMetric struct {
+	Type   *string `json:"type,omitempty"`
+	Name   *string `json:"name,omitempty"`
+	Passed *bool   `json:"passed,omitempty"`
+}
+
 const clientIdMaxLength = 128
+const securityMetricsMaxLength = 30
+
+const missingClientIdError = "missing clientId in payload"
+const invalidTimestampError = "timestamp must be a valid number"
+const missingDataError = "missing metrics data in payload"
+const securityMetricsEmptyError = "data.security cannot be empty"
+const securityMetricMissingTypeError = "invalid element in data.security at position %v, type must be included"
+const securityMetricMissingNameError = "invalid element in data.security at position %v, name must be included"
+const securityMetricMissingPassedError = "invalid element in data.security at position %v, passed must be included"
 
 var clientIdLengthError = fmt.Sprintf("clientId exceeded maximum length of %v", clientIdMaxLength)
+var securityMetricsLengthError = fmt.Sprintf("maximum length of data.security %v", securityMetricsMaxLength)
 
 func (m *Metric) Validate() (valid bool, reason string) {
 	if m.ClientId == "" {
-		return false, "missing clientId in payload"
+		return false, missingClientIdError
 	}
 
 	if len(m.ClientId) > clientIdMaxLength {
@@ -48,13 +67,33 @@ func (m *Metric) Validate() (valid bool, reason string) {
 
 	if m.ClientTimestamp != "" {
 		if _, err := m.ClientTimestamp.Int64(); err != nil {
-			return false, "timestamp must be a valid number"
+			return false, invalidTimestampError
 		}
 	}
 
 	// check if data field was missing or empty object
 	if m.Data == nil || (MetricData{}) == *m.Data {
-		return false, "missing metrics data in payload"
+		return false, missingDataError
+	}
+
+	if m.Data.Security != nil {
+		if len(*m.Data.Security) == 0 {
+			return false, securityMetricsEmptyError
+		}
+		if len(*m.Data.Security) > securityMetricsMaxLength {
+			return false, securityMetricsLengthError
+		}
+		for i, sm := range *m.Data.Security {
+			if sm.Type == nil {
+				return false, fmt.Sprintf(securityMetricMissingTypeError, i)
+			}
+			if sm.Name == nil {
+				return false, fmt.Sprintf(securityMetricMissingNameError, i)
+			}
+			if sm.Passed == nil {
+				return false, fmt.Sprintf(securityMetricMissingPassedError, i)
+			}
+		}
 	}
 	return true, ""
 }
